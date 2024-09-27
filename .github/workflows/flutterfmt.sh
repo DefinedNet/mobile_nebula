4403fef32e7f9ba6e20aa0bd22fdddcca0dbb42a
Ian VanSchooten <ian.vanschooten@gmail.com>
2024-09-27 14:02:25 -0400

    Fix flutter format command
    It was removed in 3.12: https://github.com/flutter/flutter/pull/129360

--- .github/workflows/flutterfmt.sh
+++ .github/workflows/flutterfmt.sh
@@ -3,7 +3,7 @@ DIRS="lib test"
 EXIT=0
 
 for DIR in $DIRS; do
-    OUT="$(flutter format -l 120 --suppress-analytics "$DIR" | sed -e "s/^Formatted \(.*\)/::error file=$DIR\/\1::Not formatted/g")"
+    OUT="$(dart format -l 120 --suppress-analytics "$DIR" | sed -e "s/^Formatted \(.*\)/::error file=$DIR\/\1::Not formatted/g")"
     echo "$OUT" | grep "::error" && EXIT=1
 done
 
