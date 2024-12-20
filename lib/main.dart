d02fd402dbfc5d75048bff9708806767265b0f49
Ian VanSchooten <ian.vanschooten@gmail.com>
2024-12-20 14:55:55 -0500

    Listen to system brightness changes if needed

--- lib/main.dart
+++ lib/main.dart
@@ -64,6 +64,16 @@ class _AppState extends State<App> {
       });
     });
 
+    // Listen to changes to the system brightness mode, update accordingly
+    var dispatcher = SchedulerBinding.instance.platformDispatcher;
+    dispatcher.onPlatformBrightnessChanged = () {
+      if (settings.useSystemColors) {
+        setState(() {
+          brightness = dispatcher.platformBrightness;
+        });
+      }
+    };
+
     super.initState();
   }
 
