c20bd93a9885b934b0c0691a0171cfe10cd3901c
Caleb Jasik <calebjasik@jasik.xyz>
2025-01-23 15:21:26 -0600

    Migrate from `colorScheme.background` to `colorScheme.surface`

--- lib/services/theme.dart
+++ lib/services/theme.dart
@@ -346,7 +346,7 @@ class MaterialTheme {
           bodyColor: colorScheme.onSurface,
           displayColor: colorScheme.onSurface,
         ),
-        scaffoldBackgroundColor: colorScheme.background,
+        scaffoldBackgroundColor: colorScheme.surface,
         canvasColor: colorScheme.surface,
       );
 
