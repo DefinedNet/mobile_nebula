c6cbd5c3d3a2cf79b233860cd1cd81e04dcb903c
Ian VanSchooten <ian.vanschooten@gmail.com>
2025-01-24 11:39:08 -0500

    Clean up enrollment screen

--- lib/screens/EnrollmentScreen.dart
+++ lib/screens/EnrollmentScreen.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
@@ -6,7 +7,11 @@ import 'package:flutter/services.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 import 'package:mobile_nebula/components/SimplePage.dart';
+import 'package:mobile_nebula/components/buttons/PrimaryButton.dart';
 import 'package:url_launcher/url_launcher.dart';
+
+import '../components/config/ConfigItem.dart';
+import '../components/config/ConfigSection.dart';
 
 class EnrollmentScreen extends StatefulWidget {
   final String? code;
@@ -151,30 +156,59 @@ class _EnrollmentScreenState extends State<EnrollmentScreen> {
       ]));
     }
 
-    return SimplePage(
-        title: Text('Enroll with Managed Nebula'),
-        child: Padding(child: child, padding: EdgeInsets.symmetric(horizontal: 10)),
-        alignment: alignment);
+    return SimplePage(title: Text('Enroll with Managed Nebula'), child: child, alignment: alignment);
   }
 
   Widget _codeEntry() {
+    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+    String? validator(String? value) {
+      if (value == null || value.isEmpty) {
+        return 'Code or link is required';
+      }
+      return null;
+    }
+
+    Future<void> onSubmit() async {
+      final bool isValid = _formKey.currentState?.validate() ?? false;
+      if (!isValid) {
+        return;
+      }
+
+      setState(() {
+        code = EnrollmentScreen.parseCode(enrollInput.text);
+        error = null;
+        _enroll();
+      });
+    }
+
+    final input = Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: PlatformTextFormField(
+          controller: enrollInput,
+          validator: validator,
+          hintText: 'from admin.defined.net',
+          cupertino: (_, __) => CupertinoTextFormFieldData(
+            prefix: Text("Code or link"),
+          ),
+          material: (_, __) => MaterialTextFormFieldData(
+            decoration: const InputDecoration(labelText: 'Code or link'),
+          ),
+        ));
+
+    final form = Form(
+      key: _formKey,
+      child: Platform.isAndroid ? input : ConfigSection(children: [input]),
+    );
+
     return Column(children: [
       Padding(
-          padding: EdgeInsets.only(top: 20),
-          child: PlatformTextField(
-            hintText: 'defined.net enrollment code or link',
-            controller: enrollInput,
-          )),
-      PlatformTextButton(
-        child: Text('Submit'),
-        onPressed: () {
-          setState(() {
-            code = EnrollmentScreen.parseCode(enrollInput.text);
-            error = null;
-            _enroll();
-          });
-        },
-      )
+        padding: EdgeInsets.symmetric(vertical: 32),
+        child: form,
+      ),
+      Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Row(children: [Expanded(child: PrimaryButton(child: Text('Submit'), onPressed: onSubmit))]))
     ]);
   }
 }
