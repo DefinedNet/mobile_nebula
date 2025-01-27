3ec5882b2e1f9f229a3ddfe077b2289ef87e2bd3
Caleb Jasik <calebjasik@jasik.xyz>
2025-01-27 17:21:41 -0600

    Add padding to the ConfigItem children to prevent text from touching the edge

--- lib/components/config/ConfigItem.dart
+++ lib/components/config/ConfigItem.dart
@@ -29,7 +29,7 @@ class ConfigItem extends StatelessWidget {
 
     return Container(
         color: Utils.configItemBackground(context),
-        padding: EdgeInsets.only(top: 2, bottom: 2, left: 15, right: 20),
+        padding: EdgeInsets.only(top: 10, bottom: 10, left: 15, right: 20),
         constraints: BoxConstraints(minHeight: Utils.minInteractiveSize),
         child: Row(
           crossAxisAlignment: crossAxisAlignment,
