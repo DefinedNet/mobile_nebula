5c5ed095081da54f6a8948902e8bde9dede381ef
SpiralP <spiral.palmer@gmail.com>
2025-01-30 04:17:49 -0800

    nix: update to work newer gradle

--- flake.nix
+++ flake.nix
@@ -10,19 +10,23 @@
       revSuffix = lib.optionalString (self ? shortRev || self ? dirtyShortRev)
         "-${self.shortRev or self.dirtyShortRev}";
 
-      makePackages = (pkgs:
+      makePackages = (pkgs: devShell:
         let
           androidComposition = pkgs.androidenv.composeAndroidPackages {
             includeNDK = true;
             includeEmulator = true;
-            # sdk version, from android/app/build.gradle
-            platformVersions = [ "33" "34" ];
-            platformToolsVersion = "34.0.5";
-            # needs to match version in android/app/build.gradle
-            buildToolsVersions = [ "34.0.0" ];
+
+            # needs to match compileSdkVersion in android/app/build.gradle
+            platformVersions = [ "34" ];
+            # needs to match ndkVersion in android/app/build.gradle
+            ndkVersion = "27.0.12077973";
+            # needs to match buildToolsVersion in android/app/build.gradle
+            # latest https://developer.android.com/tools/releases/build-tools
+            buildToolsVersions = [ "35.0.0" ];
+            # latest https://developer.android.com/tools/releases/platform-tools
+            platformToolsVersion = "35.0.2";
+            # latest https://developer.android.com/tools/releases/sdk-tools
             toolsVersion = "26.1.1";
-            # needs to match version in android/app/build.gradle
-            ndkVersion = "26.1.10909125";
             extraLicenses = [
               "android-googletv-license"
               "android-sdk-arm-dbt-license"
@@ -39,10 +43,11 @@
 
           androidSdk = androidComposition.androidsdk;
           platformTools = androidComposition.platform-tools;
-          jdk = pkgs.jdk;
+          jdk = lib.trivial.throwIfNot (lib.versions.major pkgs.jdk.version == "21")
+            "jdk updated to ${lib.versions.major pkgs.jdk.version}, sync android/app/build.gradle versions"
+            pkgs.jdk;
 
           flutter = pkgs.flutter;
-          gradle = pkgs.gradle;
 
           gomobile = (pkgs.gomobile.override {
             androidPkgs = androidComposition;
@@ -51,6 +56,7 @@
               src = pkgs.fetchFromGitHub {
                 owner = "golang";
                 repo = "mobile";
+                # needs to match golang.org/x/mobile version in nebula/go.mod
                 rev = "c31d5b91ecc32c0d598b8fe8457d244ca0b4e815";
                 hash = "sha256-SD+/QGJejtqAkAdbd8kg7MON9Yg/0qQEKO8RfxI+1bg=";
               };
@@ -84,11 +90,31 @@
                   cat $out
                 ''
             );
-            hash = "sha256-GUcXRCV1pvluHBvvosMOmk/JD3Adeu4z64ebeef/BcA=";
+            hash = "sha256-KriNbeLCPmra5zY65uKcvdKnCemSkptItlMP0McTO9Y=";
           };
+
+          setup = ''
+            export GOCACHE="$TMPDIR/go-cache"
+            export GOPATH="$TMPDIR/go"
+
+            # flutter sdk's .gradle/ dir needs to be writable,
+            # or else gradle exit 1's silently
+            rm -rf "$TMPDIR/flutter-sdk"
+            cp -r ${pkgs.flutter.sdk} "$TMPDIR/flutter-sdk"
+            chmod -c u+w "$TMPDIR/flutter-sdk/packages/flutter_tools/gradle/.gradle"
+            # update FLUTTER_ROOT or else nix store flutter rewrites its own sdk path in local.properties
+            export FLUTTER_ROOT="$TMPDIR/flutter-sdk"
+
+            cat <<EOF > android/local.properties
+            sdk.dir=$ANDROID_SDK_ROOT
+            ndk.dir=$ANDROID_NDK_ROOT
+            flutter.sdk=$TMPDIR/flutter-sdk
+            EOF
+            cat android/local.properties
+          '';
         in
         rec {
-          inherit self gomobile androidComposition platformTools androidStudio jdk gradle flutter;
+          inherit self gomobile androidComposition platformTools androidStudio jdk flutter;
 
           sign = pkgs.writeShellApplication {
             name = "sign";
@@ -120,17 +146,51 @@
             });
           };
 
+          oss_licenses = (pkgs.flutter.buildFlutterApplication {
+            pname = "oss_licenses";
+            version = "0.1.0" + revSuffix;
+
+            src = ./.;
+
+            autoPubspecLock = ./pubspec.lock;
+
+            buildPhase = ''
+              chmod -c u+w .dart_tool/package_config.json
+              flutter pub run flutter_oss_licenses:generate.dart --output $out
+              cat $out
+            '';
+
+            targetFlutterPlatform = "universal";
+
+            dontDartBuild = true;
+            dontDartInstall = true;
+            dontDartInstallCache = true;
+
+            outputHash = "sha256-oCVLNdsJfwyepCij4BN6XQvWTyI0PgIlXgkgzVy0ETc=";
+            outputHashAlgo = "sha256";
+            outputHashMode = "flat";
+          }).overrideAttrs (prev: {
+            outputs = [ "out" ];
+          });
+
+          updateDeps = default.mitmCache.updateScript;
+
           default = pkgs.flutter.buildFlutterApplication rec {
             pname = "mobile_nebula";
             version = "0.1.0" + revSuffix;
 
             src = ./.;
 
+            autoPubspecLock = ./pubspec.lock;
+
             postPatch = ''
               substituteInPlace gen-artifacts.sh \
                 --replace-fail \
-                'git rev-parse --short HEAD' \
-                'echo ${self.shortRev or self.dirtyShortRev}'
+                  'git rev-parse --short HEAD' \
+                  'echo ${self.shortRev or self.dirtyShortRev}' \
+                --replace-fail \
+                  'flutter pub run flutter_oss_licenses:generate.dart' \
+                  'cat ${oss_licenses} > lib/oss_licenses.dart'
 
               ${lib.getExe pkgs.sd} \
                 'version: .+\+(.+)' \
@@ -142,28 +202,30 @@
               || exit 1
             '';
 
-            autoPubspecLock = ./pubspec.lock;
+            targetFlutterPlatform = "universal";
 
             ANDROID_HOME = "${androidSdk}/libexec/android-sdk";
             ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
             ANDROID_NDK_ROOT = "${ANDROID_HOME}/ndk-bundle";
-            GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_HOME}/build-tools/${buildToolsVersion}/aapt2";
             JAVA_HOME = jdk.home;
-
-            gradleFlags = [
-              "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_HOME}/build-tools/${buildToolsVersion}/aapt2"
-            ];
+            GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_HOME}/build-tools/${buildToolsVersion}/aapt2";
+            gradleFlags = [ GRADLE_OPTS ];
 
             nativeBuildInputs = with pkgs; [
-              # add flutter, or else we build with a minimalFlutter, without bin/cache/artifacts/engine/android-arm64-release/linux-x64/gen_snapshot
+              # add flutter, or else we build with a minimalFlutter, 
+              # which doesn't have needed bin/cache/artifacts/engine/android-arm64-release/linux-x64/gen_snapshot
               flutter
+
+              # add gradle for gradleConfigureHook (gradleFlags gradleFlagsArray below),
+              # and mitmCache gradleUpdateScript,
+              # even though we don't use this version of gradle to build (we use the wrapper)
+              gradle
 
               clang
               dart
               gcc
               go
               gomobile
-              gradle
               jdk
               platformTools
               sdkmanager
@@ -172,17 +234,19 @@
               androidStudio
             ];
 
+            dontDartBuild = true;
+            dontDartInstall = true;
             dontUseCmakeConfigure = true;
             dontUseNinjaBuild = true;
-            dontUseNinjaInstall = true;
             dontUseNinjaCheck = true;
+            dontUseNinjaInstall = true;
 
             preGradleUpdate = "cd android";
             postGradleUpdate = "cd ..";
 
             gradleUpdateTask = ":app:minifyReleaseWithR8 :app:lintVitalReportRelease";
 
-            mitmCache = gradle.fetchDeps {
+            mitmCache = pkgs.gradle.fetchDeps {
               # to update, run in non-develop shell:
               # nix build --print-build-logs .#app.mitmCache.updateScript && ./result
               data = ./deps.json;
@@ -199,21 +263,15 @@
 
               export HOME=$TMPDIR
 
-              export GOCACHE=$TMPDIR/go-cache
-              export GOPATH="$TMPDIR/go"
-
               touch env.sh
 
               patchShebangs gen-artifacts.sh
 
-              cat <<EOF > android/local.properties
-              sdk.dir=$ANDROID_SDK_ROOT
-              ndk.dir=$ANDROID_NDK_ROOT
-              flutter.sdk=${pkgs.flutter.sdk}
-              EOF
+              ${setup}
 
               # Substitute the gradle-all zip URL by a local file to prevent downloads from happening while building an Android app
               ${lib.getExe pkgs.sd} 'distributionUrl=.+' 'distributionUrl=file\://${gradleZip}' android/gradle/wrapper/gradle-wrapper.properties
+              cat android/gradle/wrapper/gradle-wrapper.properties
 
               # sets up gradlew wrapper
               flutter build apk -v --config-only
@@ -231,6 +289,7 @@
               wrapProgram "$gradlewPath" \
                 --run 'set -x' \
                 --add-flags '--info' \
+                --add-flags '--debug' \
                 "''${wrapperFlagsArray[@]}"
 
               gradle() {
@@ -256,7 +315,7 @@
               runHook postInstall
             '';
 
-            shellHook = ''
+            shellHook = lib.optionalString devShell ''
               set -ex
 
               flutter --version
@@ -266,18 +325,13 @@
                 exit 1
               fi
 
-              export GOCACHE=$TMPDIR/go-cache
-              export GOPATH="$TMPDIR/go"
+              ${setup}
 
-              cat <<EOF > android/local.properties
-              sdk.dir=$ANDROID_SDK_ROOT
-              ndk.dir=$ANDROID_NDK_ROOT
-              flutter.sdk=${pkgs.flutter.sdk}
-              EOF
-              cat android/local.properties
+              rm -vrf .idea
+              rm -vf android/.gradle/config.properties
+              rm -vf android/gradlew android/gradlew.bat android/gradle/wrapper/gradle-wrapper.jar
 
-              rm -rf .idea/libraries
-
+              # sets up gradlew wrapper
               flutter build apk -v --config-only
             '';
           };
@@ -298,12 +352,10 @@
               ];
             };
           };
-
-          packages = makePackages pkgs;
         in
         {
-          devShells.${system} = packages;
-          packages.${system} = packages;
+          devShells.${system} = makePackages pkgs true;
+          packages.${system} = makePackages pkgs false;
         })
       lib.systems.flakeExposed);
 }
