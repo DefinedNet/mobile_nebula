05f29fd814bd909a903e3da757e4cd159edb417f
Nate Brown <nate@defined.net>
2026-02-03 21:17:17 -0600

    On ChromeOS, bring dns resolvers from the underlay network into the vpn

--- android/app/src/main/kotlin/net/defined/mobile_nebula/NebulaVpnService.kt
+++ android/app/src/main/kotlin/net/defined/mobile_nebula/NebulaVpnService.kt
@@ -14,6 +14,9 @@ import androidx.core.content.ContextCompat
 import androidx.work.*
 import mobileNebula.CIDR
 import java.io.File
+import java.net.Inet4Address
+import java.net.Inet6Address
+import java.net.InetAddress
 
 
 class NebulaVpnService : VpnService() {
@@ -146,6 +149,19 @@ class NebulaVpnService : VpnService() {
         // RCS / Jibe
         disallowApp(builder, "com.google.android.apps.messaging")
 
+        if (isChromeOs()) {
+            // Newer versions of ChromeOS need a dns server installed for resolution to work at all.
+            // Even if the system is configured to use DoH in which case the resolvers here will may be entirely ignored.
+            // We are only considering public dns resolvers at the moment to avoid network roaming possibly
+            // breaking dns resolution.
+            // A proper setup would be to allow the user to configure the dns resolvers but this would still have use
+            // as a fallback in case none were configured.
+            getPublicDnsResolvers().forEach {
+                Log.i(TAG, "Hoisting dns resolver into the vpn: $it")
+                builder.addDnsServer(it)
+            }
+        }
+
         try {
             vpnInterface = builder.establish()
             nebula = mobileNebula.MobileNebula.newNebula(site!!.config, site!!.getKey(this), site!!.logFile, vpnInterface!!.detachFd().toLong())
@@ -270,6 +286,26 @@ class NebulaVpnService : VpnService() {
             Log.e(TAG, "$err")
         }
         send(msg, id)
+    }
+
+    private fun isChromeOs(): Boolean {
+        val pm = packageManager
+        return pm.hasSystemFeature("org.chromium.arc") || pm.hasSystemFeature("org.chromium.arc.device_management")
+    }
+
+    private fun getPublicDnsResolvers(): List<String> {
+        val connectivityManager = getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = connectivityManager.activeNetwork ?: return emptyList()
+        val linkProperties = connectivityManager.getLinkProperties(activeNetwork) ?: return emptyList()
+
+        val dnsResolvers = mutableListOf<String>()
+        linkProperties.dnsServers.forEach {
+            val address = it.hostAddress
+            if (address != null && it.isGlobalUnicast()) {
+                dnsResolvers.add(address)
+            }
+        }
+        return dnsResolvers
     }
 
     inner class ReloadReceiver : BroadcastReceiver() {
@@ -402,4 +438,35 @@ class NebulaVpnService : VpnService() {
         messenger = Messenger(IncomingHandler())
         return messenger.binder
     }
+}
+
+fun InetAddress.isGlobalUnicast(): Boolean {
+    // Must not be link-local, loopback, or multicast
+    if (this.isLinkLocalAddress || this.isLoopbackAddress || this.isMulticastAddress || this.isSiteLocalAddress) {
+        return false
+    }
+
+    if (this is Inet4Address) {
+        val address = this.hostAddress
+
+        // Exclude Private IPv4 ranges (10.0.0.0/8, 172.16.0.0/12, 192.168.0.0/16)
+        return address != null &&
+                address != "0.0.0.0" &&
+                !isIpv4Private(this.address)
+    } else if (this is Inet6Address) {
+        return true
+    }
+
+    return false
+}
+
+fun isIpv4Private(addr: ByteArray): Boolean {
+    if (addr.size < 4) {
+        return true
+    }
+
+    val b = addr[0].toInt() and 0xFF
+    return b == 10 ||
+            (b == 172 && (addr[1].toInt() and 0xFF) in 16..31) ||
+            (b == 192 && (addr[1].toInt() and 0xFF) == 168)
 }
